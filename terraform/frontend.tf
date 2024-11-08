7d078b64363b89292329844877576375d8e1ecb7
Prughvi <prudhviraj3525@gmail.com>
2024-11-08 15:53:15 +0530

    commit

---
+++ terraform/frontend.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0b308c7134616a7ce"]
+  tags ={
+    Name = "frontend"
+  }
+}
